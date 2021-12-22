de1a7d065d31bbdd223cbc6ce17905276563e519
Lemi Eba <lemieba@gmail.com>
2021-12-22 13:36:33 -0500

    Added all the basic necessities

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ The computer will detect when they win.
 
 - Minimum requirement
   - [x] It has to have a the tic tac toe table.
-  - [ ] first player will be x and second player will be o.
-  - [ ] when an empty grid is clicked the current player's x or o will be shown onto that grid and the current player will switch. 
-  - [ ] a non empty grid should no be clicked or switched.
-  - [ ] As the game goes on after every click check if this click creates a winner.
+  - [x] first player will be x and second player will be o.
+  - [x] when an empty grid is clicked the current player's x or o will be shown onto that grid and the current player will switch. 
+  - [x] a non empty grid should no be clicked or switched.
+  - [x] As the game goes on after every click check if this click creates a winner.
 
 
--- main.js
+++ main.js
@@ -1,9 +1,83 @@
-// set player state
-
 // value representint X and O
 const X = 1
-const O = 2
+const O = -1
 
+const toString = {
+    [X]: "X",
+    [O]: "O"
+}
+
+
+// set initial state
 current_player = X
+board = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+]
 
 
+
+// helper functions
+/**
+ * how to check if there is a winner
+ * 
+ * input: 
+ * output: boolean
+ * 
+ * goal
+ * does the current board have a winner
+ * 
+ * solution:
+ * check if any of the rows are winners
+ * chekc if any of the columns are winners
+ * check if any of the diagonals are winners
+ * 
+ * 
+ */
+function winner() {
+    // check rows
+    for (let r = 0; r < board.length; r++) {
+        const row = board[r];
+        if (Math.abs(row[0] + row[1] + row[2]) === 3) {
+            return true;
+        }
+    }
+
+    // check columns
+    for (let c = 0; c < board[0].length; c++) {
+        const column = board[0][c];
+        if (Math.abs(column[0] + column[1] + column[2]) === 3) {
+            return true;
+        }
+    }
+
+    // check diagonals
+    if (Math.abs(board[0][0] + board[1][1] + board[2][2]) === 3) {
+        return true;
+    }
+    if (Math.abs(board[0][2] + board[1][1] + board[2][0]) === 3) {
+        return true;
+    }
+
+    return false;
+}
+
+const cells = document.querySelectorAll('td');
+cells.forEach(cell => {
+    cell.addEventListener('click', () => {
+        const r = cell.closest('tr').rowIndex; 
+        const c = cell.cellIndex;
+
+        console.log(r, c);
+        if (board[r][c] == 0) {
+            var text = document.createTextNode(toString[current_player]);
+            board[r][c] = current_player;
+            cell.appendChild(text);
+            if (winner()){
+                alert("player " + toString[current_player] +  " has won");
+            }
+            current_player = (current_player == X) ? O : X;
+        } 
+    });
+})
